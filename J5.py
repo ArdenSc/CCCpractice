8b0fdb84e1515430a71d61a03a5f9fe256540e05
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2019-11-19 21:02:10 -0600

    Completed J5, output system missing

--- J5.py
+++ J5.py
@@ -1,5 +1,4 @@
 rules = []
-solutions = []
 
 
 def split_string(string):
@@ -23,11 +22,11 @@ def find_subs(string):
         while True:
             location = string.find(rule[0], index)
             if location != -1:
-                index = location + len(string)
+                index = location + len(rule[0])
                 locations.append([i + 1, location + 1, string[:location] + rule[1] + string[location + len(rule[0]):]])
             else:
                 break
-    return locations
+    return locations if locations != [] else False
 
 
 with open("inputJ5.txt", 'r') as data:
@@ -37,5 +36,27 @@ with open("inputJ5.txt", 'r') as data:
             rules.append(split_string(line))
         elif lineNum == 3:
             steps, initial, final = split_string(line)
+            steps = int(steps)
 
 
+solutions = [{"start": [0, 0, 0, initial]}]
+
+
+def find_sol(section):
+    for part in section:
+        tmp = find_subs(part["start"][3])
+        if tmp:
+            part["end"] = []
+            for solution in tmp:
+                if part["start"][0] + 1 == steps:
+                    if solution[2] == final:
+                        print(solution)
+                        exit()
+                elif part["start"][0] + 1 < steps:
+                    part["end"].append({"start": [part["start"][0] + 1] + solution})
+                else:
+                    print("error")
+            find_sol(part["end"])
+
+
+find_sol(solutions)
